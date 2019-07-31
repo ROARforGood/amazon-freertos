28048371bb03d05ef6cd1cacfcbcf64467ecd489
Sarena Meas <sarem@amazon.com>
2019-07-31 12:08:43 -0700

    saving work.

--- libraries/c_sdk/standard/https/test/unit/iot_tests_https_async.c
+++ libraries/c_sdk/standard/https/test/unit/iot_tests_https_async.c
@@ -24,13 +24,73 @@
  */
 
 /**
- * @file iot_tests_https_sync.c
- * @brief Tests for IotHttpsClient_SendAsync(), IotHttpsClient_WriteRequestBody(), and IotHttpsClient_ReadResponseBody()
+ * @file iot_tests_https_async.c
+ * @brief Tests for IotHttpsClient_SendAsync().
  * in iot_https_client.h.
  */
 
 #include "iot_tests_https_common.h"
 #include "platform/iot_clock.h"
+
+/**
+ * @brief Timeout for all requests to finish on a single connection.
+ */
+#define HTTPS_TEST_ASYNC_TIMEOUT_MS                     ( (uint32_t) 30000 )
+
+/**
+ * @brief The maximum number of asynchronous requests on the same connection. 
+ * 
+ * This is used to separate the user buffers needed for each request.
+ */
+#define HTTPS_TEST_MAX_ASYNC_REQUESTS                   ( 3 )
+
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief HTTPS response and request user buffers for scheduling multiple requests.
+ */
+static uint8_t _pAsyncRespUserBuffers[HTTPS_TEST_MAX_ASYNC_REQUESTS][HTTPS_TEST_RESP_USER_BUFFER_SIZE] = { 0 };
+static uint8_t _pAsyncReqUserBuffers[HTTPS_TEST_MAX_ASYNC_REQUESTS][HTTPS_TEST_REQ_USER_BUFFER_SIZE] = { 0 };
+
+/**
+ * @brief HTTPS request and response information configrations for sheduling multiple requests.
+ */
+static IotHttpsRequestInfo_t _pAsyncReqConfigs[HTTPS_TEST_MAX_ASYNC_REQUESTS] = {0};
+
+/**
+ * @brief A base IotHttpsAsyncInfo_t to copy to each of the request information configurations for each request.
+ * 
+ * The tests will replace callbacks in .callbacks as needed and add pPrivData as need for the test.
+ */
+static IotHttpsAsyncInfo_t _asyncInfoBase = {
+    .callbacks = { 0 },     /* To be updated during TEST_SETUP and as needed during testing. */
+    .pPrivData = NULL       /* To be updated as needed during testing. */
+};
+
+/**
+ * @brief A base IotHttpsRequestInfo_t to copy to each of the request information configurations for each request.
+ */
+static IotHttpsRequestInfo_t _reqInfoBase = {
+    .pPath = HTTPS_TEST_PATH,
+    .pathLen = sizeof( HTTPS_TEST_PATH ) - 1,
+    .method = IOT_HTTPS_METHOD_GET,
+    .pHost = HTTPS_TEST_ADDRESS,
+    .hostLen = sizeof( HTTPS_TEST_ADDRESS ) - 1,
+    .isNonPersistent = false,
+    .userBuffer.pBuffer = NULL, /* To be updated during TEST_SETUP. */
+    .userBuffer.bufferLen = 0,  /* To be updated during TEST_SETUP. */
+    .isAsync = true,
+    .u.pAsyncInfo = &_asyncInfoBase
+};
+
+/**
+ * @brief A IotHttpsResponseInfo_t for an asynchronous response.
+ */
+static IotHttpsResponseInfo_t _respInfoBase = {
+    .userBuffer.pBuffer = NULL, /* To be updated during TEST_SETUP */
+    .userBuffer.bufferLen = 0,  /* To be updated during TEST_SETUP */
+    .pSyncInfo = NULL
+};
 
 /*-----------------------------------------------------------*/
 
@@ -46,6 +106,7 @@ TEST_GROUP( HTTPS_Client_Unit_Async );
  */
 TEST_SETUP( HTTPS_Client_Unit_Async )
 {
+
     /* This will initialize the library before every test case, which is OK. */
     TEST_ASSERT_EQUAL_INT( true, IotSdk_Init() );
     TEST_ASSERT_EQUAL( IOT_HTTPS_OK, IotHttpsClient_Init());
@@ -66,5 +127,17 @@ TEST_TEAR_DOWN( HTTPS_Client_Unit_Async )
 
 TEST_GROUP_RUNNER( HTTPS_Client_Unit_Async )
 {
+    RUN_TEST_CASE( HTTPS_Client_Unit_Async, SendAsyncInvalidParameters );
 }
 
+/*-----------------------------------------------------------*/
+
+TEST( HTTPS_Client_Unit_Async, SendAsyncInvalidParameters )
+{
+    IotHttpsReturnCode_t returnCode = IOT_HTTPS_OK;
+    IotHttpsRequestHandle_t reqHandle = IOT_HTTPS_REQUEST_HANDLE_INITIALIZER;
+    IotHttpsConnectionHandle_t connHandle = IOT_HTTPS_CONNECTION_HANDLE_INITIALIZER;
+    IotHttpsResponseHandle_t respHandle = IOT_HTTPS_RESPONSE_HANDLE_INITIALIZER;
+
+
+}
